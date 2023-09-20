9f324a45943344d6f72dc28674004210436cd2a2
NGODAT0103 <21521935@gm.uit.edu.vn>
2023-09-20 21:55:57 +0700

    test linh tinh

---
+++ app/src/main/java/me/ngodat0103/myapplication/MainActivity.java
@@ -0,0 +1,28 @@
+package me.ngodat0103.myapplication;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+import android.widget.EditText;
+import android.widget.TextView;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+    public void onBtnClick(View view){
+        EditText edtFirstName = findViewById(R.id.editFirstName);
+        EditText editLastName = findViewById(R.id.editLastName);
+        EditText editEmail = findViewById(R.id.editEmail);
+        TextView viewFirstName = findViewById(R.id.texFirstName);
+        TextView viewLastName = findViewById(R.id.textLastName);
+        TextView viewEmail = findViewById(R.id.textEmail);
+        viewFirstName.setText(edtFirstName.getText().toString());
+        viewLastName.setText(editLastName.getText());
+        viewEmail.setText(editEmail.getText());
+    }
+}
