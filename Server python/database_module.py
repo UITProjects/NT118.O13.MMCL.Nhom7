07905eacf6a1817e11855b246e301729a389b31e
NGODAT0103 <21521935@gm.uit.edu.vn>
2023-09-24 22:40:20 +0700

    thêm database_module kết nối mariadb python server

---
+++ Server python/database_module.py
@@ -0,0 +1,22 @@
+import mysql.connector
+import os
+USERNAME = "ngovuminhdat"
+PASSWORD = "123456789"
+HOST = "app.mariadb.uitprojects.com"
+PORT = 3306
+DATABASE = "mobile_project"
+SSL_CERT = os.getenv("ssl_client_cert")
+SSL_key = os.getenv("ssl_client_key")
+print(SSL_CERT,SSL_key)
+connect_mysql = mysql.connector.connect(user=USERNAME, password=PASSWORD,
+                                  host=HOST,
+                                  database='mobile_project',
+                                  ssl_cert=SSL_CERT,
+                                  ssl_key=SSL_key,
+                                        )
+if connect_mysql.is_connected():
+    print("connected")
+execute_command_interpreter = connect_mysql.cursor()
+execute_command_interpreter.execute("SHOW TABLES;")
+print(execute_command_interpreter.fetchall())
+connect_mysql.close()
