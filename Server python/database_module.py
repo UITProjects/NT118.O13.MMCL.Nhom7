5a46f5ceddea1e6668bd805a5226a63d6f3e6a4c
NGODAT0103 <21521935@gm.uit.edu.vn>
2023-09-24 23:35:34 +0700

    thêm tính năng hash password cho database

--- Server python/database_module.py
+++ Server python/database_module.py
@@ -1,5 +1,6 @@
 import mysql.connector
 import os
+from hash_password_module import hash_password
 USERNAME = os.getenv("username_mysql")
 PASSWORD = os.getenv("password_mysql")
 HOST = "app.mariadb.uitprojects.com"
@@ -17,6 +18,11 @@ connect_mysql = mysql.connector.connect(user=USERNAME, password=PASSWORD,
 if connect_mysql.is_connected():
     print("connected")
 execute_command_interpreter = connect_mysql.cursor()
-execute_command_interpreter.execute("SHOW TABLES;")
+#execute_command_interpreter.execute("SHOW TABLES;")
+hashed_password = hash_password("mypassword")
+insert_command = (f"INSERT INTO `mobile_project`.`account`(`username_primary`,`hashed_password`)VALUES('test_username',"
+                  f"'{hashed_password}');")
+execute_command_interpreter.execute(insert_command)
 print(execute_command_interpreter.fetchall())
+connect_mysql.commit()
 connect_mysql.close()
