d921a8d65ff3e0034033e05143416cd1c90165c9
LAPTOP-CV1CRQMN\acer <21522243@gm.uit.edu.vn>
2023-09-30 21:52:51 +0700

    no message

--- module java/java client connect/src/Main.java
+++ module java/java client connect/src/Main.java
@@ -10,7 +10,7 @@ public class Main {
     public static void main(String[] args) throws IOException, InvalidAlgorithmParameterException, NoSuchPaddingException, IllegalBlockSizeException, NoSuchAlgorithmException, BadPaddingException, InvalidKeyException {
            client_connection_module.client_connection_module_init("localhost",2509);
             handle_request_types_module.authentication("test_account1","123456789");
-            handle_request_types_module.forgot_password("test_account2","ngovuminhdat@gmail.com");
+            handle_request_types_module.forgot_password("test_account1","phuckien289@gmail.com");
 //            handle_request_types_module.upload_image_profile("test_account1");
 //            handle_request_types_module.upload_image_profile("test_account2");
     }
