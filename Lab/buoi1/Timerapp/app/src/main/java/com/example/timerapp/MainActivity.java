b27d8c2703eb47fd943beab9b258db7648bd28c9
NGODAT0103 <21521935@gm.uit.edu.vn>
2023-10-16 21:14:24 +0700

    Thêm tính năng cho chọn thời gian cụ thể

--- Lab/buoi1/Timerapp/app/src/main/java/com/example/timerapp/MainActivity.java
+++ Lab/buoi1/Timerapp/app/src/main/java/com/example/timerapp/MainActivity.java
@@ -7,8 +7,8 @@ import android.os.Bundle;
 import android.os.CountDownTimer;
 import android.view.View;
 import android.widget.Button;
+import android.widget.NumberPicker;
 import android.widget.ProgressBar;
-import android.widget.SeekBar;
 import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,6 +19,9 @@ public class MainActivity extends AppCompatActivity {
     Boolean counterIsActive = false;
     ProgressBar timer_pg;
     MediaPlayer mediaPlayer;
+    NumberPicker npHours;
+    NumberPicker npMinutes;
+    NumberPicker npSeconds;
 
     public long TimeLeft = 30000;
     @Override
@@ -32,6 +35,16 @@ public class MainActivity extends AppCompatActivity {
         start_btn = findViewById(R.id.start_btn);
         reset_btn = findViewById(R.id.reset_btn);
         mediaPlayer = MediaPlayer.create(getApplicationContext(), R.raw.alarm);
+        npHours = findViewById(R.id.npHours);
+        npMinutes = findViewById(R.id.npMinutes);
+        npSeconds = findViewById(R.id.npSeconds);
+        npSeconds.setMinValue(0);
+        npSeconds.setMaxValue(60);
+        npMinutes.setMinValue(0);
+        npMinutes.setMaxValue(60);
+        npHours.setMinValue(0);
+        npHours.setMaxValue(24);
+        npSeconds.setValue(10);
         reset_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
@@ -43,8 +56,13 @@ public class MainActivity extends AppCompatActivity {
 
 
     private void update(int progress){
-        int minutes = progress / 60;
-        int seconds = progress % 60;
+        int hours = progress/60/60;
+        int minutes = progress/60%60;
+        int seconds = progress;
+        if (seconds==0 && minutes>=1)
+            minutes-=1;
+        if (minutes==0 && hours>=1)
+            hours-=1;
         String secondsFinal = "";
         if(seconds <= 9){
             secondsFinal = "0" + seconds;
@@ -53,13 +71,24 @@ public class MainActivity extends AppCompatActivity {
             secondsFinal = "" + seconds;
         }
         timer_pg.setProgress(progress);
-        timer_tv.setText("" + minutes + ":" + secondsFinal);
+        timer_tv.setText(hours+":" + minutes + ":" + secondsFinal);
     }
 
     public void start_timer(View view){
         if(counterIsActive == false){
             counterIsActive = true;
             start_btn.setText("STOP");
+            TimeLeft = 0 ;
+            TimeLeft += (long) npHours.getValue()*60*60;
+            TimeLeft += (long) npMinutes.getValue()*60;
+            TimeLeft += (long) npSeconds.getValue();
+            timer_pg.setMax(((int)TimeLeft));
+            TimeLeft = TimeLeft*1000;
+            timer_pg.setProgress(0);
+            npHours.setVisibility(View.GONE);
+            npMinutes.setVisibility(View.GONE);
+            npSeconds.setVisibility(View.GONE);
+            timer_tv.setVisibility(View.VISIBLE);
             countDownTimer = new CountDownTimer(TimeLeft, 1000) {
                 @Override
                 public void onTick(long l) {
@@ -106,9 +135,11 @@ public class MainActivity extends AppCompatActivity {
         counterIsActive = false;
     }
     public void TimeReset(){
-        TimeLeft = 30000;
         counterIsActive = false;
-        timer_tv.setText("00:30");
+        timer_tv.setVisibility(View.GONE);
+        npHours.setVisibility(View.VISIBLE);
+        npMinutes.setVisibility(View.VISIBLE);
+        npSeconds.setVisibility(View.VISIBLE);
     }
     @Override
     protected void onPause() {
