8cdd6949de692edec911a9f54c7f877249be5fb6
NGODAT0103 <21521935@gm.uit.edu.vn>
2023-10-29 14:46:07 +0700

    Sắp xếp lại tổng hợp các layout và xoá mấy thứ linh tinh không cần thiết

--- Android/app/src/main/java/com/example/mobileproject/Login.java
+++ Android/app/src/main/java/com/example/mobileproject/Login.java
@@ -3,6 +3,7 @@ package com.example.mobileproject;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.Button;
 
 public class Login extends AppCompatActivity {
 
@@ -10,5 +11,6 @@ public class Login extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
     }
 }
