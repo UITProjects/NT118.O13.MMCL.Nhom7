3290b3d785ebfda699a5fcbef5baa99ae3a0dc94
NGODAT0103 <21521935@gm.uit.edu.vn>
2023-10-31 23:03:53 +0700

    test Webview: chưa xong

--- Android/app/src/main/java/com/example/mobileproject/Register.java
+++ Android/app/src/main/java/com/example/mobileproject/Register.java
@@ -2,13 +2,25 @@ package com.example.mobileproject;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class Register extends AppCompatActivity {
 
+    Button signup_button;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_register);
+        signup_button = findViewById(R.id.btn_signup);
+        signup_button.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                Intent webview = new Intent(getApplicationContext(),WebviewRegister.class);
+                startActivity(webview);
+            }
+        });
     }
 }
