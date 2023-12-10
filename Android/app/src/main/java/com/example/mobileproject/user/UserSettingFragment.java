809772b8f18cfdc7d963cc9b6093ecedb24e1cc4
LAPTOP-CV1CRQMN\acer <21522243@gm.uit.edu.vn>
2023-12-10 13:43:54 +0700

    update UI and API to UserSettingFragment

--- Android/app/src/main/java/com/example/mobileproject/user/UserSettingFragment.java
+++ Android/app/src/main/java/com/example/mobileproject/user/UserSettingFragment.java
@@ -9,9 +9,20 @@ import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
+import android.widget.TextView;
 import android.widget.Toast;
 
+import com.example.mobileproject.APIClient;
+import com.example.mobileproject.APIInterface;
 import com.example.mobileproject.R;
+import com.google.gson.Gson;
+
+import org.json.JSONException;
+import org.json.JSONObject;
+
+import retrofit2.Call;
+import retrofit2.Callback;
+import retrofit2.Response;
 
 /**
  * A simple {@link Fragment} subclass.
@@ -21,6 +32,8 @@ import com.example.mobileproject.R;
 public class UserSettingFragment extends Fragment {
     View view;
     Button logout_btn;
+    TextView name, lastname, firstname, username, email;
+    APIInterface apiInterface;
 
     @Override
     public void onResume() {
@@ -40,6 +53,54 @@ public class UserSettingFragment extends Fragment {
         // Inflate the layout for this fragment
         view =  inflater.inflate(R.layout.fragment_usersetting, container, false);
         logout_btn = view.findViewById(R.id.btn_logout);
+        name = view.findViewById(R.id.textView3);
+        lastname = view.findViewById(R.id.lastname);
+        firstname = view.findViewById(R.id.firstname);
+        username = view.findViewById(R.id.username);
+        email = view.findViewById(R.id.email);
+        apiInterface = APIClient.getClient().create(APIInterface.class);
+        Call User = apiInterface.getUser();
+        User.enqueue(new Callback() {
+            @Override
+            public void onResponse(Call call, Response response) {
+                Log.d("API Call", response.code() + "");
+                if (response.isSuccessful() && response.body() != null) {
+                    Log.d("API Call", "Successful");
+                    Log.d("API Call", response.toString());
+                    Log.d("API Call", "response: " + new Gson().toJson(response.body()));
+                    try {
+                        JSONObject jsonObject = new JSONObject(new Gson().toJson(response.body()));
+                        String userValue = jsonObject.getString("username");
+                        String firstnameValue = jsonObject.getString("firstName");
+                        String lastnameValue = jsonObject.getString("lastName");
+                        String emailValue = jsonObject.getString("email");
+                        if(firstnameValue == null || lastnameValue == null || firstnameValue == null && lastnameValue == null){
+                            name.setText(userValue);
+                            username.setText(userValue);
+                            firstname.setText(firstnameValue);
+                            lastname.setText(lastnameValue);
+                            email.setText(emailValue);
+                        } else {
+                            name.setText(firstnameValue + " " + lastnameValue);
+                            username.setText(userValue);
+                            firstname.setText(firstnameValue);
+                            lastname.setText(lastnameValue);
+                            email.setText(emailValue);
+                        }
+                    } catch (JSONException e) {
+                        throw new RuntimeException(e);
+                    }
+                }
+            }
+
+            @Override
+            public void onFailure(Call call, Throwable t) {
+                Log.d("API CALL", t.getMessage().toString());
+
+                //t.printStackTrace();
+
+            }
+        });
         logout_btn.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
